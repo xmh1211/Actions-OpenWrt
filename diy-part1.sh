4a1c16b0c2b169bfcee4c060273d427bf7da30c5
xmh1211 <157801666+xmh1211@users.noreply.github.com>
2024-11-22 14:08:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,5 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
