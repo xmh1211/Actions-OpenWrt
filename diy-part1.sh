0234a99a3b2a1bf99e247657a9c3262eb752b9b7
xmh1211 <157801666+xmh1211@users.noreply.github.com>
2025-05-17 23:11:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,14 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 修改路由器IP为192.168.100.1
+sed -i "s/$192.168.1.1/$192.168.100.1/g" package/base-files/files/bin/config_generate
+
+# 修改路由器主机名为X-WRT
+sed -i "s/$hostname='ImmortalWrt'/$hostname='X-WRT'/g" package/base-files/files/bin/config_generate
+
+# 修改时区为 CST-8
+sed -i "s/$timezone='UTC'/$timezone='CST-8'/g" package/base-files/files/bin/config_generate
+
+# 修改地区为上海
+sed -i '/CST-8/a \\t\tset system.@system[-1].zonename='Asia/Shanghai'' package/base-files/files/bin/config_generate
